e72faf4d754821bee8b53755081369fc1c0352ad
sangam shinde <sangampshinde@gmail.com>
2024-06-29 00:04:46 +0530

    file upload

--- CreateFilephp/createfile-data-from-html-form.php
+++ CreateFilephp/createfile-data-from-html-form.php
@@ -1,4 +1,28 @@
+<form method="post">
+    <input type="text" placeholder="Enter file name" name="file_name">
+    <br><br>
+    <textarea name="content"></textarea>
+    <br><br>
+    <button>Create File</button>
+</form>
 <?php
+
+if(isset($_POST['file_name'])) {
+
+$filename = "Files/".$_POST['file_name'];
+$content = $_POST['content'];
+
+$file = fopen($filename, "w");
+
+fwrite($file, $content);
+
+fclose($file);
+
+echo"file created";
+
+
+}
+
 
 
 
