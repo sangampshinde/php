befe2181f99b4ed4abe43a4452f69a56d7f60e90
sangam shinde <sangampshinde@gmail.com>
2024-06-29 16:05:56 +0530

    list file

--- FileOperation/ReadFiles/readfile.php
+++ FileOperation/ReadFiles/readfile.php
@@ -1,5 +1,16 @@
 <?php
 
+$filename = "../Files/data.txt";
+
+$fileopen = fopen($filename,"r")or die("unable read");//it takes two parameters (filename , method for file[read,write,update])
+
+$data =fread($fileopen, filesize($filename));// it takes two parameters (opened_file_variable ,filesize());
+
+echo $data;
+
+fclose($fileopen);
+
+
 
 
 
