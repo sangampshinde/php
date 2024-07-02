63a8b5f33425ba032d869453ec4831d9e3002154
sangam shinde <sangampshinde@gmail.com>
2024-07-02 11:34:54 +0530

    files

--- FileOperation/ListFilefromFolder/listfilefromfolder.php
+++ FileOperation/ListFilefromFolder/listfilefromfolder.php
@@ -1,4 +1,14 @@
 <?php
+$path="Files";
+// toscan directory use function called scandir()
+$item=scandir($path);
+print_r($item);
+// Array ( [0] => . [1] => .. [2] => data.txt [3] => dummy.txt [4] => test )
+$item=array_diff($item,array(".",".."));
+echo"<br>";
+print_r($item);
+//Array ( [2] => data.txt [3] => dummy.txt [4] => test )
+
 
 
 
